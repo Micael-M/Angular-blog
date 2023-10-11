1a3ed7279fab8eedb067f08f8d337ee2c321a267
Micael Santos <micaelmaicon@hotmail.com>
2023-10-10 21:05:17 -0300

    style: concluo layout base do main-card component

--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-main-card',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-card.component.css']
 })
 export class MainCardComponent {
-
+  @Input()
+  imageCover:string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  cardDescription:string = ""
 }
